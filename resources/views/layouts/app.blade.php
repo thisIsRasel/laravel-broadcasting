c5e66bc6adda0f1a8685b197d1bb9f99b51ab623
Rasel Ahammed <raselcse20@gmail.com>
2018-02-06 00:44:10 +0600

    laravel broadcasting change 2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -56,18 +56,6 @@
                             <li><a href="{{ route('login') }}">Login</a></li>
                             <li><a href="{{ route('register') }}">Register</a></li>
                         @else
-
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true"><span class="glyphicon glyphicon-envelope"></span></a>
-
-                                <ul class="dropdown-menu">
-                                    <li>
-                                        <a href="#">First User</a>
-                                        <a href="#">Second User</a>
-                                    </li>
-                                </ul>
-                            </li>
-
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
                                     {{ Auth::user()->name }} <span class="caret"></span>
